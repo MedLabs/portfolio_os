6cd07fbe22296cf90f2e459d89dcad10e1c91552
Medlabs <mlbsaad@gmail.com>
2024-11-14 15:31:17 +0100

    Add tooltip

--- src/lib/components/Panel.svelte
+++ src/lib/components/Panel.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Tooltip from './Tooltip.svelte';
 	import Icon from '@iconify/svelte';
 
 	const { apps, toggleApp } = $props<{
@@ -11,10 +12,12 @@
 	class="flex justify-center gap-4 items-center fixed bottom-3 w-1/2 rounded-xl backdrop-blur-sm h-20 bg-white/30 text-center z-max"
 >
 	{#each apps as app}
-		<Icon
-			icon={app.icon}
-			class="text-6xl hover:scale-150 hover:-translate-y-6 duration-200"
-			onclick={() => toggleApp(app.value)}
-		/>
+		<Tooltip text={app.title}>
+			<Icon
+				icon={app.icon}
+				class="text-6xl hover:scale-150 hover:-translate-y-6 duration-200"
+				onclick={() => toggleApp(app.value)}
+			/>
+		</Tooltip>
 	{/each}
 </div>
---
+++ src/lib/components/Tooltip.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	let { children, text } = $props<{ children: () => void; text: string }>();
+
+	let visible = $state(false);
+
+	let tooltipRef = $state<HTMLDivElement | null>(null);
+	let targetRef = $state<HTMLDivElement | null>(null);
+
+	// Event listeners for mouse and focus events
+	onMount(() => {
+		if (targetRef) {
+			targetRef.addEventListener('mouseenter', showTooltip);
+			targetRef.addEventListener('mouseleave', hideTooltip);
+			targetRef.addEventListener('focus', showTooltip);
+			targetRef.addEventListener('blur', hideTooltip);
+		}
+	});
+
+	// Cleanup event listeners when the component is destroyed
+	onDestroy(() => {
+		if (targetRef) {
+			targetRef.removeEventListener('mouseenter', showTooltip);
+			targetRef.removeEventListener('mouseleave', hideTooltip);
+			targetRef.removeEventListener('focus', showTooltip);
+			targetRef.removeEventListener('blur', hideTooltip);
+		}
+	});
+
+	const showTooltip = () => (visible = true);
+	const hideTooltip = () => (visible = false);
+</script>
+
+<div class="relative inline-block" bind:this={targetRef}>
+	{#if visible}
+		<div
+			bind:this={tooltipRef}
+			class="absolute left-1/2 transform -translate-x-1/2 mb-8 p-2 bg-gray-700 text-white text-sm rounded bottom-full"
+		>
+			{text}
+		</div>
+	{/if}
+	{@render children()}
+</div>

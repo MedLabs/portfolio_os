8047c98009f15813cb10b454a696a85c9756c5a3
Medlabs <mlbsaad@gmail.com>
2024-11-16 16:39:57 +0100

    update /help cmd

--- src/lib/utils/shell.ts
+++ src/lib/utils/shell.ts
@@ -4,15 +4,20 @@ export function shell(cmd: string): string {
   }
   switch (cmd.trim()) {
     case "/help":
-      return "Available commands: whoami, ls, cat, /files, /email, /blog, /clear";
+      return `Available commands:<br>
+              whoami : returns infos about me<br>
+              ls : lists files in home directory<br>
+              cat : displays file's content<br>
+              /files : runs Files Browser<br>
+              /email : runs Contact form<br>
+              /blog : runs blog app<br>
+              /clear : clears terminal history`;
     case "whoami":
-      return "Medlabs, Fullstack Developer, Web Scraper, SysAdmin since 2004";
+      return "Hi Medlabs here, a Fullstack Developer, Web Scraper, SysAdmin since 2004...";
     case "ls":
       return "projects.md &emsp; passwords.txt &emsp; fsociety.v &emsp; cat.png";
     case "cat":
       return "usage: cat 'filename'";
-    case "clear":
-      return "C";
     case "cat passwords.txt":
       return "facebook: 12345678<br>twitter:wrongpassword<br>reddit: freePal3$Tine<br>paypal: billiondollaraccount6969";
     case "cat fsociety.txt":
